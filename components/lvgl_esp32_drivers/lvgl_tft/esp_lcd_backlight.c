71355f3c0ffc24358a2444f7cd1c12c3cfe71801
yjrqz777 <3210551161@qq.com>
2024-03-14 12:55:04 +0800

    update driver fun

--- components/lvgl_esp32_drivers/lvgl_tft/esp_lcd_backlight.c
+++ components/lvgl_esp32_drivers/lvgl_tft/esp_lcd_backlight.c
@@ -10,6 +10,7 @@
 #include "driver/ledc.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+
 #include "soc/ledc_periph.h" // to invert LEDC output on IDF version < v4.3
 #include "soc/gpio_sig_map.h"
 typedef struct {
